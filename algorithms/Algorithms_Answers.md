eb4b6087a300df114e3d2d8b0c0f1e6de3ffe40c
Naaz Shaikh <naazhome@Naazs-MacBook-Pro-3.local>
2018-10-13 05:20:16 -0700

    solution

--- algorithms/Algorithms_Answers.md
+++ algorithms/Algorithms_Answers.md
@@ -1,1 +1,9 @@
 Add your answers to the Algorithms exercises here.
+
+a) O(n)
+b) O(n^3)
+c) O(logn)
+
+Exercise 2
+
+A binary search : O(log n)
--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -1,46 +1,75 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass    
 
-  def breadth_first_for_each(self, cb):
-    pass
+# recursive solution
+    # def depth_first_for_each(self, cb):
+    #     cb(self.value)
+    #     if self.left:
+    #         self.left.depth_first_for_each(cb)
+    #     if self.right:
+    #         self.right.depth_first_for_each(cb)
 
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if (value < self.value):
-      if not self.left:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if not self.right:
-        self.right = new_tree
-      else:
-        self.right.insert(value)
+# or this iterative solution
 
-  def contains(self, target):
-    if self.value == target:
-      return True
-    if self.left:
-      if self.left.contains(target):
-        return True
-    if self.right:
-      if self.right.contains(target):
-        return True
-    return False
 
-  def get_max(self):
-    if not self:
-      return None
-    max_value = self.value
-    current = self
-    while current:
-      if current.value > max_value:
-        max_value = current.value
-      current = current.right
-    return max_value
+    def depth_first_for_each(self, cb):
+        stack = []
+        stack.append(self)
+
+        while len(stack):
+            current_node = stack.pop()
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+            cb(current_node.value)
+
+    def breadth_first_for_each(self, cb):
+        queue = []
+        queue.append(self)
+        while len(queue):
+            current_node = queue.pop(0)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+            cb(current_node.value)
+
+    def insert(self, value):
+        new_tree = BinarySearchTree(value)
+        if (value < self.value):
+            if not self.left:
+                self.left = new_tree
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = new_tree
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.left:
+            if self.left.contains(target):
+                return True
+        if self.right:
+            if self.right.contains(target):
+                return True
+        return False
+
+    def get_max(self):
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
